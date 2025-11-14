152d348cd40d5c398c6b9aacffa5d67e8ff9b64a
Team 3 <evugrant@deloitte.ie>
2025-11-14 14:35:43 +0000

    Create script monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in {1..3}
+do
+    echo "Running Iteration $i"
+    top -b -n 1
+    if $i != 3 then {echo "---------------------------------------------------------"}fi
+done
+
+
